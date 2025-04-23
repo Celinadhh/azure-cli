0f0055bd685db588ed30a6330aedae898f56e78e
Celina Zhao <zhaocelina@microsoft.com>
2025-04-23 16:19:35 -0700

    wip

--- src/azure-cli/azure/cli/command_modules/cdn/aaz/latest/afd/profile/_delete.py
+++ src/azure-cli/azure/cli/command_modules/cdn/aaz/latest/afd/profile/_delete.py
@@ -54,6 +54,11 @@ class Delete(AAZCommand):
         _args_schema.resource_group = AAZResourceGroupNameArg(
             required=True,
         )
+        
+        _args_schema.with_policy_token = AAZBoolArg(
+            options=["--with-policy-token"],
+            help="Indicates that a policy token should be acquired and included before deleting the AFD profile."
+        )
         return cls._args_schema
 
     def _execute_operations(self):
@@ -63,7 +68,9 @@ class Delete(AAZCommand):
 
     @register_callback
     def pre_operations(self):
-        pass
+        if self.ctx.args.with_policy_token:
+            response = self.AcquirePolicyToken(ctx=self.ctx)()
+            self.ctx.policy_token = response.get("token")
 
     @register_callback
     def post_operations(self):
@@ -108,13 +115,89 @@ class Delete(AAZCommand):
         @property
         def url(self):
             return self.client.format_url(
-                "/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.Cdn/profiles/{profileName}",
+                "/subscriptions/{subscriptionId}/providers/Microsoft.Authorization/acquirePolicyToken",
                 **self.url_parameters
             )
 
         @property
         def method(self):
             return "DELETE"
+
+        @property
+        def error_format(self):
+            return "MgmtErrorFormat"
+
+        @property
+        def url_parameters(self):
+            parameters = {
+                **self.serialize_url_param(
+                    "subscriptionId", self.ctx.subscription_id,
+                    required=True,
+                ),
+            }
+            return parameters
+
+        @property
+        def query_parameters(self):
+            parameters = {
+                **self.serialize_query_param(
+                    "api-version", "2024-09-01",
+                    required=True,
+                ),
+            }
+            return parameters
+        
+        @property
+        def content(self):
+            operation_uri = {
+                f"https://management.azure.com/subscriptions/{self.ctx.subscription_id}"
+                f"/resourceGroups/{self.ctx.args.resource_group}/providers/Microsoft.Cdn.profiles/{self.ctx.args.profile_name}"
+                "?api-version=2025-04-15"
+            }
+
+            return {
+                "operation": {
+                    "uri": operation_uri,
+                    "httpMethod": "DELETE"
+                }
+            }
+
+        @property
+        def header_parameters(self):
+            parameters = {
+                **self.serialize_header_param(
+                    "x-ms-policy-external-evaluations", self.ctx.policy_token if self.ctx.args.with_policy_tok else {},
+                ),
+            }
+            return parameters
+        
+        def on_200(self, session):
+            pass
+
+        def on_204(self, session):
+            pass
+    
+    class AcquirePolicyToken(AAZHttpOperation):
+        CLIENT_TYPE = "MgmtClient"
+        
+        def __call__(self, *args, **kwargs):
+            request = self.make_request()
+            session = self.client.send_request(request=request, stream=False, **kwargs)
+            if session.http_response.status_code in [200]:
+                return self.on_200(session)
+            
+            return self.on_error(session.http_response)
+
+        @property
+        def url(self):
+            return self.client.format_url(
+                "/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.Cdn/profiles/{profileName}",
+                **self.url_parameters
+            )
+
+        @property
+        def method(self):
+            return "POST"
 
         @property
         def error_format(self):
@@ -142,18 +225,26 @@ class Delete(AAZCommand):
         def query_parameters(self):
             parameters = {
                 **self.serialize_query_param(
-                    "api-version", "2024-09-01",
+                    "api-version", "2025-03-01",
                     required=True,
                 ),
             }
             return parameters
-
+        
+        @property
+        def header_parameters(self):
+            parameters = {
+                **self.serialize_header_param(
+                    "Content-Type", "application/json",
+                ),
+            }
+            return parameters
+        
         def on_200(self, session):
             pass
 
         def on_204(self, session):
             pass
-
 
 class _DeleteHelper:
     """Helper class for Delete"""
