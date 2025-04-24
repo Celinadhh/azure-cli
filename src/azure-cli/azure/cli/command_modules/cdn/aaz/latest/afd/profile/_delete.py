fdb69bbc259f5c9766471502903e6e83a2ca4054
Celina Zhao <zhaocelina@microsoft.com>
2025-04-24 13:05:06 -0700

    fix

--- src/azure-cli/azure/cli/command_modules/cdn/aaz/latest/afd/profile/_delete.py
+++ src/azure-cli/azure/cli/command_modules/cdn/aaz/latest/afd/profile/_delete.py
@@ -59,6 +59,11 @@ class Delete(AAZCommand):
             options=["--with-policy-token"],
             help="Indicates that a policy token should be acquired and included before deleting the AFD profile."
         )
+
+        _args_schema.policy_token = AAZStrArg(
+            nullable=True
+        )
+
         return cls._args_schema
 
     def _execute_operations(self):
@@ -70,7 +75,8 @@ class Delete(AAZCommand):
     def pre_operations(self):
         if self.ctx.args.with_policy_token:
             response = self.AcquirePolicyToken(ctx=self.ctx)()
-            self.ctx.policy_token = response.get("token")
+            token = response.get("token")
+            print("Successfully received token, passing into header for profile delete")
 
     @register_callback
     def post_operations(self):
@@ -115,89 +121,13 @@ class Delete(AAZCommand):
         @property
         def url(self):
             return self.client.format_url(
-                "/subscriptions/{subscriptionId}/providers/Microsoft.Authorization/acquirePolicyToken",
-                **self.url_parameters
-            )
-
-        @property
-        def method(self):
-            return "DELETE"
-
-        @property
-        def error_format(self):
-            return "MgmtErrorFormat"
-
-        @property
-        def url_parameters(self):
-            parameters = {
-                **self.serialize_url_param(
-                    "subscriptionId", self.ctx.subscription_id,
-                    required=True,
-                ),
-            }
-            return parameters
-
-        @property
-        def query_parameters(self):
-            parameters = {
-                **self.serialize_query_param(
-                    "api-version", "2024-09-01",
-                    required=True,
-                ),
-            }
-            return parameters
-        
-        @property
-        def content(self):
-            operation_uri = {
-                f"https://eastus2euap.management.azure.com/subscriptions/{self.ctx.subscription_id}"
-                f"/resourceGroups/{self.ctx.args.resource_group}/providers/Microsoft.Cdn.profiles/{self.ctx.args.profile_name}"
-                "?api-version=2025-04-15"
-            }
-
-            return {
-                "operation": {
-                    "uri": operation_uri,
-                    "httpMethod": "DELETE"
-                }
-            }
-
-        @property
-        def header_parameters(self):
-            parameters = {
-                **self.serialize_header_param(
-                    "x-ms-policy-external-evaluations", self.ctx.policy_token if self.ctx.args.with_policy_tok else {},
-                ),
-            }
-            return parameters
-        
-        def on_200(self, session):
-            pass
-
-        def on_204(self, session):
-            pass
-    
-    class AcquirePolicyToken(AAZHttpOperation):
-        CLIENT_TYPE = "MgmtClient"
-        
-        def __call__(self, *args, **kwargs):
-            request = self.make_request()
-            session = self.client.send_request(request=request, stream=False, **kwargs)
-            if session.http_response.status_code in [200]:
-                return self.on_200(session)
-            
-            return self.on_error(session.http_response)
-
-        @property
-        def url(self):
-            return self.client.format_url(
                 "/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.Cdn/profiles/{profileName}",
                 **self.url_parameters
             )
 
         @property
         def method(self):
-            return "POST"
+            return "DELETE"
 
         @property
         def error_format(self):
@@ -225,6 +155,87 @@ class Delete(AAZCommand):
         def query_parameters(self):
             parameters = {
                 **self.serialize_query_param(
+                    "api-version", "2024-09-01",
+                    required=True,
+                ),
+            }
+            return parameters
+        
+        @property
+        def header_parameters(self):
+            if not self.ctx.args.with_policy_token:
+                return {}
+            
+            parameters = {
+                **self.serialize_header_param(
+                    "x-ms-policy-external-evaluations", self.ctx.args.policy_token
+                ),
+            }
+            return parameters
+
+        def on_200(self, session):
+            pass
+
+        def on_204(self, session):
+            pass
+    
+    class AcquirePolicyToken(AAZHttpOperation):
+        CLIENT_TYPE = "MgmtClient"
+        
+        def __call__(self, *args, **kwargs):
+            print("Making request...")
+            request = self.make_request()
+            print(f"Request made with URL: {request.url} and Method: {self.method}")
+            session = self.client.send_request(request=request, stream=False, **kwargs)
+            
+            # Send the request
+            try:
+                session = self.client.send_request(request=request, stream=False, **kwargs)
+                print(f"Request sent. Received status code: {session.http_response.status_code}")
+            except Exception as e:
+                print(f"An error occurred while sending the request: {e}")
+                raise
+
+            # Check status code
+            if session.http_response.status_code == 200:
+                print("Handling 200 response...")
+                return self.on_200(session)
+            
+            print("Handling error response...")
+            return self.on_error(session.http_response)
+
+        @property
+        def url(self):
+            # format_url uses management.azure.com and acquirePolicyToken isn't available :(
+            url = f"https://eastus2euap.management.azure.com/subscriptions/{self.ctx.subscription_id}/providers/Microsoft.Authorization/acquirePolicyToken"
+            return url
+
+        @property
+        def method(self):
+            return "POST"
+
+        @property
+        def error_format(self):
+            return "MgmtErrorFormat"
+        
+        @property
+        def content(self):
+            operation_uri = f"https://eastus2euap.management.azure.com/subscriptions/{self.ctx.subscription_id}/resourceGroups/{self.ctx.args.resource_group}/providers/Microsoft.Cdn/profiles/{self.ctx.args.profile_name}?api-version=2025-04-15"
+
+            content = {
+                "operation": {
+                    "uri": operation_uri,
+                    "httpMethod": "DELETE"
+                },
+                "changeReference": f"/subscriptions/{self.ctx.subscription_id}/resourceGroups/{self.ctx.args.resource_group}/providers/Microsoft.ChangeSafety/changeStates/zhaocelinac2/stageProgressions/breakglassStage"
+            }
+            print(f"Content: {content}")
+            return content
+        
+        @property
+        def query_parameters(self):
+            parameters = {
+                **self.serialize_query_param(
                     "api-version", "2025-03-01",
                     required=True,
                 ),
@@ -241,8 +252,8 @@ class Delete(AAZCommand):
             return parameters
         
         def on_200(self, session):
-            pass
-
+            return self.deserialize_http_content(session)
+        
         def on_204(self, session):
             pass
 
